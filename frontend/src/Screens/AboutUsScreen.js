f20b245d9b4b7d7aa90ee7cc4ff226639d35ca60
Sabin Subedi <sabinsubedi125@gmail.com>
2021-08-26 21:31:35 +0545

    added we reach photo

--- frontend/src/Screens/AboutUsScreen.js
+++ frontend/src/Screens/AboutUsScreen.js
@@ -7,29 +7,35 @@ import NavBar from "../components/Navbar";
 import Team from "../components/Team";
 
 function AboutUsScreen() {
-    const team= useRef()
-    const hash = window.location.hash.split('#')[1]
-    const about = useRef()
-    
-    useEffect(() => {
-        hash === 'team' ? team.current.scrollIntoView() : about.current.scrollIntoView()
-    }, [hash])
-   
+  const team = useRef();
+  const hash = window.location.hash.split("#")[1];
+  const about = useRef();
+
+  useEffect(() => {
+    hash === "team"
+      ? team.current.scrollIntoView()
+      : about.current.scrollIntoView();
+  }, [hash]);
+
   return (
     <div>
-        <ScrollToTop color="#198754" smooth />
+      <ScrollToTop color="#198754" smooth />
       <NavBar />
       <Container className="py-5">
         <Col ref={about} className="py-5 mt-5 text-center px-5">
           <h1>About WeReach</h1>
-          <img src="/wereach.png" alt="Wereach" style={{maxWidth: '100%'}}/>
-          <p className='fs-5 text-secondary mt-4'>
+          <img
+            src="https://storage.googleapis.com/civism-1d244.appspot.com/image-1629992754963.png"
+            alt="Wereach"
+            style={{ maxWidth: "100%" }}
+          />
+          <p className="fs-5 text-secondary mt-4">
             Everyone has a part of them that hopes for a better world. Helping
             someone, fixing a community, or even changing a nation. We at
             WeREACH think that everyone should be able to benefit from your
             inspiration. We do this because it's the only way change happens.{" "}
           </p>
-          <p className='fs-5 text-secondary'>
+          <p className="fs-5 text-secondary">
             As a result, we make it easy for people to be inspired and put their
             compassion into action through our website. We've established a
             community of donors, sponsors and volunteers and helped project
@@ -38,18 +44,15 @@ function AboutUsScreen() {
             unreached in all over the world.
           </p>
         </Col>
-        
-     
       </Container>
       <div className="features">
         <FeatureBox />
       </div>
       <div ref={team}>
-
         <Team />
       </div>
 
-        <Footer />
+      <Footer />
     </div>
   );
 }
